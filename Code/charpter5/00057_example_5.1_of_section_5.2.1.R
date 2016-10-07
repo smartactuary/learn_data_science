1ccae8b8d111794b50561674f30c2a10414893c7
linzetao <linzetao88@163.com>
2016-10-07 13:01:21 +0800

    finishe chapter 5

--- Code/charpter5/00057_example_5.1_of_section_5.2.1.R
+++ Code/charpter5/00057_example_5.1_of_section_5.2.1.R
@@ -1,19 +1,32 @@
 # example 5.1 of section 5.2.1 
 # (example 5.1 of section 5.2.1)  : Choosing and evaluating models : Evaluating models : Evaluating classification models 
 # Title: Building and applying a logistic regression spam model 
+rm(list=ls())
+setwd('D:\\study\\r\\learn_data_science\\Data\\charpter5')
 
 spamD <- read.table('spamD.tsv',header=T,sep='\t')
+head(spamD)
+summary(spamD)
+hist(spamD$rgroup)
+
+str(spamD)
+
 spamTrain <- subset(spamD,spamD$rgroup>=10)
 spamTest <- subset(spamD,spamD$rgroup<10)
 spamVars <- setdiff(colnames(spamD),list('rgroup','spam'))
+
 spamFormula <- as.formula(paste('spam=="spam"',
    paste(spamVars,collapse=' + '),sep=' ~ '))
+
 spamModel <- glm(spamFormula,family=binomial(link='logit'),
    data=spamTrain)
+
 spamTrain$pred <- predict(spamModel,newdata=spamTrain,
    type='response')
+
 spamTest$pred <- predict(spamModel,newdata=spamTest,
    type='response')
+
 print(with(spamTest,table(y=spam,glmPred=pred>0.5)))
 ##           glmPred
 ## y          FALSE TRUE
